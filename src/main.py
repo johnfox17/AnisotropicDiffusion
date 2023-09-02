1c1a339574aa6673de5a937125e56de3712e58a1
johnfox17 <johnfox17@gmail.com>
2023-09-02 12:33:53 -0700

    Included necessary changes to main

--- src/main.py
+++ src/main.py
@@ -11,24 +11,23 @@ import cv2
 l1=1
 l2=1
 #N = 100
-dx = 1/512
-dy = 1/512
-#dt = 1/512
-#deltaX = 0.006015
-#deltaY = 0.006015
-
+dx = 1/300
+dy = 1/300
+dt = 1/300
+horizon = 3.015
+deltaX = horizon*dx
+deltaY = horizon*dy
 #t0 = 0
 #tf = 5
 #numTimeSteps =int(tf/dt)
-xCoords = np.linspace(0.5/300,1+0.5/300,300)#create the discrete x and y grids
-yCoords = np.linspace(0.5/300,1+0.5/300,300) #create the discrete x and y grids
+xCoords = np.linspace(dx/2,1+dx,300)#create the discrete x and y grids
+yCoords = np.linspace(dy/2,1+dy,300) #create the discrete x and y grids
 indexing = 'xy'
 xCoords, yCoords = np.meshgrid(xCoords, yCoords, indexing=indexing)
 xCoords = xCoords.reshape(-1, 1)
 yCoords = yCoords.reshape(-1, 1)
 coords = np.array([xCoords[:,0], yCoords[:,0]]).T
 numNodes = len(xCoords)
-print(np.shape(coords))
 #np.savetxt('/home/doctajfox/Documents/Thesis_Research/AnisotropicDiffusion/data/coords.csv', coords, delimiter=",")
 #np.savetxt('C:\\Users\\docta\\Documents\\Thesis\\AnisotropicDiffusion\\data\\coords.csv', coords, delimiter=",")
 #print('Done')
@@ -56,12 +55,12 @@ def main():
     #a = input('').split(" ")[0]
     noisyImage = np.add(image,noise)
     
-    method1 = paperDiscretization.paperDiscretization(noisyImage, coords)
-    method1.solve()
-    deNoisedImage = method1.deNoisedImage
+    #method1 = paperDiscretization.paperDiscretization(noisyImage, coords)
+    #method1.solve()
+    #deNoisedImage = method1.deNoisedImage
 
 
-    method2 = PDDODiscretization.PDDODiscretization()
+    method2 = PDDODiscretization.PDDODiscretization(numNodes, coords, dx,dy, dt, deltaX, deltaY, horizon)
     method2.solve()
 
     # display the array of pixels as an image
